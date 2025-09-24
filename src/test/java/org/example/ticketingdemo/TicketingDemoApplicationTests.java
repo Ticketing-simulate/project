6338f52b6d4ef35c8ed43083ea87391086bef640
HY-WG <mung94c@gmail.com>
2025-09-24 15:42:46 +0900

    first commit

---
+++ src/test/java/org/example/ticketingdemo/TicketingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ticketingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
