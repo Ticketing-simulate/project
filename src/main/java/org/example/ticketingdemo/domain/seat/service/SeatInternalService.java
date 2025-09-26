a216a517f86a70ccb2960a36ecf177b5f432c5b8
ksg <kasaga234@gmail.com>
2025-09-26 11:57:10 +0900

    ticket read 기능 추가

--- src/main/java/org/example/ticketingdemo/domain/seat/service/SeatInternalService.java
+++ src/main/java/org/example/ticketingdemo/domain/seat/service/SeatInternalService.java
@@ -2,10 +2,11 @@ package org.example.ticketingdemo.domain.seat.service;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
-import org.example.ticketingdemo.domain.concert.repository.ConcertRepository;
 import org.example.ticketingdemo.domain.seat.dto.request.SeatBuyRequest;
+import org.example.ticketingdemo.domain.seat.dto.request.SeatCancelRequest;
 import org.example.ticketingdemo.domain.seat.dto.response.SeatBuyResponse;
-
+import org.example.ticketingdemo.domain.seat.dto.response.SeatCancelResponse;
+import org.example.ticketingdemo.domain.seat.dto.response.SeatConcertResponse;
 import org.example.ticketingdemo.domain.seat.dto.response.SeatUserResponse;
 import org.example.ticketingdemo.domain.seat.entity.Seat;
 import org.example.ticketingdemo.domain.seat.enums.SeatStatus;
@@ -16,21 +17,22 @@ import org.example.ticketingdemo.domain.user.entity.User;
 import org.example.ticketingdemo.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class SeatInternalService {
     private final SeatRepository seatRepository;
     private final UserRepository userRepository;
-    private final ConcertRepository concertRepository;
 
-    @Transactional // Buy
+    @Transactional // 좌석 구매
     public SeatBuyResponse buySeat(SeatBuyRequest seatBuyRequest, Long userId, Long concertId) {
         // 유저 조회
-        User user = userRepository.findById(userId).orElseThrow(() -> new InvaildSeatException(SeatErrorCode.SEAT_NOT_FOUND_USER_ID));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new InvaildSeatException(SeatErrorCode.SEAT_NOT_FOUND_USER_ID));
 
-        // Concert에서 생성된 좌석 조회
-        Seat seat = seatRepository.findBySeatNumber(seatBuyRequest.seatNumber()).orElseThrow(() -> new InvaildSeatException(SeatErrorCode.SEAT_NOT_FOUND_SEAT));
+        Seat seat = seatRepository.findByConcertIdAndSeatNumber(concertId, seatBuyRequest.seatNumber())
+                .orElseThrow(() -> new InvaildSeatException(SeatErrorCode.SEAT_NOT_FOUND_SEAT));
 
         // 이미 팛린 좌석인지 확인
         if(seat.getStatus() == SeatStatus.SOLD) {
@@ -38,32 +40,30 @@ public class SeatInternalService {
         }
         seat.assignUser(user); // 유저와 연결
         seat.changeStatus(SeatStatus.SOLD); // 구매 되어 seat 상태 sold 변경
-        return SeatBuyResponse.from(seat, SeatUserResponse.from(seat));
+        return SeatBuyResponse.from(seat, SeatUserResponse.from(seat), SeatConcertResponse.from(seat));
     }
-    @Transactional
-    public SeatBuyResponse cancelSeat(Long seatId, Long userId) {
+
+    @Transactional // 좌석 구매 취소
+    public SeatCancelResponse cancelSeat(SeatCancelRequest seatCancelRequest, Long userId, Long concertId) {
         // 유저 조회
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new InvaildSeatException(SeatErrorCode.SEAT_NOT_FOUND_USER_ID));
 
-        // 좌석 조회
-        Seat seat = seatRepository.findById(seatId)
-                .orElseThrow(() -> new InvaildSeatException(SeatErrorCode.SEAT_NOT_FOUND_USER_ID));
+        // 콘서트 + 좌석 번호로 좌석 조회
+        Seat seat = seatRepository.findByConcertIdAndSeatNumber(concertId, seatCancelRequest.seatNumber())
+                .orElseThrow(() -> new InvaildSeatException(SeatErrorCode.SEAT_NOT_FOUND_SEAT));
 
-        // 이미 판매되지 않았다면 취소 불가
-        if (seat.getStatus() != SeatStatus.SOLD) {
-            throw new InvaildSeatException(SeatErrorCode.SEAT_NOT_SOLD);
-        }
+        seat.cancelBy(user);
 
-        // 좌석 구매자가 본인인지 확인
-        if (!seat.getUser().getId().equals(user.getId())) {
-            throw new InvaildSeatException(SeatErrorCode.SEAT_NOT_MATCH_USER);
-        }
+        return SeatCancelResponse.from(seat, SeatConcertResponse.from(seat));
+    }
 
-        // 취소 처리
-        seat.assignUser(null);
-        seat.changeStatus(SeatStatus.AVAILABLE);
-
-        return SeatBuyResponse.from(seat, null);
+    // 팔리지 않은(ACAILABLE)한 좌석만 출력
+    @Transactional(readOnly = true)
+    public List<String> getAvailableSeatNumbers(Long concertId) {
+        return seatRepository.findAllByConcertIdAndStatus(concertId, SeatStatus.AVAILABLE)
+                .stream()
+                .map(Seat::getSeatNumber) // seatNumber는 int
+                .toList();
     }
 }
