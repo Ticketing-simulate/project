e62dfd5464ce9e60a14775c92a0fe78ebbcd7917
hanjsnote <109492030+hanjsnote@users.noreply.github.com>
2025-09-26 12:30:56 +0900

    Update PaymentCreateRequest.java

--- src/main/java/org/example/ticketingdemo/domain/payment/dto/request/PaymentCreateRequest.java
+++ src/main/java/org/example/ticketingdemo/domain/payment/dto/request/PaymentCreateRequest.java
@@ -2,14 +2,7 @@ package org.example.ticketingdemo.domain.payment.dto.request;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
-public class PaymentCreateRequest {
+public record PaymentCreateRequest(@Min(1) @NotNull Long ticketId) {
 
-    @Min(1)
-    @NotNull
-    private final Long ticketId;
 }
