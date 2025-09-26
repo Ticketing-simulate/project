cb5316bd8f734cf18153929bd92ea2767fbb5406
Kangsunggyu <162697203+Kangsunggyu@users.noreply.github.com>
2025-09-26 19:47:54 +0900

    Revert "Feat/seat status"

--- src/main/java/org/example/ticketingdemo/domain/payment/dto/request/PaymentCreateRequest.java
+++
@@ -1,8 +0,0 @@
-package org.example.ticketingdemo.domain.payment.dto.request;
-
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-
-public record PaymentCreateRequest(@Min(1) @NotNull Long ticketId) {
-
-}
--- src/main/java/org/example/ticketingdemo/domain/seat/repository/SeatRepository.java
+++ src/main/java/org/example/ticketingdemo/domain/seat/repository/SeatRepository.java
@@ -3,23 +3,10 @@ package org.example.ticketingdemo.domain.seat.repository;
 import org.example.ticketingdemo.domain.seat.entity.Seat;
 import org.example.ticketingdemo.domain.seat.enums.SeatStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface SeatRepository extends JpaRepository<Seat,Long> {
-    @Query("SELECT s FROM Seat s " +
-            "JOIN FETCH s.concert c " +
-            "LEFT JOIN FETCH s.user u " +
-            "WHERE c.concertId = :concertId AND s.seatNumber = :seatNumber")
-    Optional<Seat> findByConcertIdAndSeatNumber(Long concertId, String seatNumber);
-
-
-
-    @Query("SELECT s FROM Seat s " +
-            "JOIN FETCH s.concert c " +
-            "LEFT JOIN FETCH s.user u " +
-            "WHERE c.concertId = :concertId AND s.status = :status")
-    List<Seat> findAllByConcertIdAndStatus(Long concertId, SeatStatus status);
+    Optional<Seat> findBySeatNumber(String seatNumber);
 }
